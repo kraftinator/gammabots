83578b2e1b29dc6eaa2cfe3675d9f981cff61488
Adam Kraft <adamkraft1@gmail.com>
2025-01-21 14:02:25 -0600

    Created ethers_adapter.js

--- app/services/ethers_service.rb
+++ app/services/ethers_service.rb
@@ -1,14 +1,28 @@
 require 'open3'
+require 'json'
 
 class EthersService
-  NODE_SCRIPT_PATH = Rails.root.join('lib', 'node_scripts', 'ethers_script.js')
+  NODE_SCRIPT_PATH = Rails.root.join('lib', 'node_scripts', 'ethers_adapter.js')
 
-  def self.get_balance(address, provider_url)
-    command = "node #{NODE_SCRIPT_PATH} #{address} #{provider_url}"
+  def self.call_function(function_name, *args)
+    command = ["node", NODE_SCRIPT_PATH.to_s, function_name, *args].join(' ')
     stdout, stderr, status = Open3.capture3(command)
 
-    raise "Error: #{stderr.strip}" unless status.success?
+    if status.success?
+      response = JSON.parse(stdout)
+      return response['result'] if response['success']
 
-    stdout.strip # Return the balance
+      raise "Error from Node script: #{response['error']}"
+    else
+      raise "Node script execution failed: #{stderr.strip}"
+    end
+  end
+
+  def self.get_balance(address, provider_url)
+    call_function('getBalance', address, provider_url)
+  end
+
+  def self.get_transaction_count(address, provider_url)
+    call_function('getTransactionCount', address, provider_url)
   end
 end
--- lib/tasks/ethers_test.rake
+++ lib/tasks/ethers_test.rake
@@ -1,4 +1,5 @@
 namespace :ethers_test do
+
   desc "Get balance"
   task :get_balance, [:wallet] => :environment do |t, args|
     if args[:wallet].nil?
@@ -9,4 +10,16 @@ namespace :ethers_test do
     balance = EthersService.get_balance(args[:wallet], "https://base-mainnet.infura.io/v3/#{infura_api_key}")
     puts balance
   end
+
+  desc "Get nonce"
+  task :get_transaction_count, [:wallet] => :environment do |t, args|
+    if args[:wallet].nil?
+      raise ArgumentError, "Missing parameters!"
+    end
+    puts "wallet = #{args[:wallet]}"
+    infura_api_key = ENV['INFURA_API_KEY']
+    balance = EthersService.get_transaction_count(args[:wallet], "https://base-mainnet.infura.io/v3/#{infura_api_key}")
+    puts balance
+  end
+
 end
