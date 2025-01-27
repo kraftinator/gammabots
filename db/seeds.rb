c8539c98457fb946986c3ceaf4542ab599c96c86
Adam Kraft <adamkraft1@gmail.com>
2025-01-27 10:27:11 -0600

    Initialize tokens

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,39 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-Chain.find_or_create_by!(
+base_chain = Chain.find_or_create_by!(
   name: "Base",
   native_chain_id: "8453",
   rpc_url: "https://base-mainnet.infura.io/v3",
   explorer_url: "https://basescan.org"
+)
+
+weth_token = Token.find_or_create_by!(
+  chain: base_chain,
+  symbol: "WETH",
+  name: "Wrapped Ether",
+  contract_address: "0x4200000000000000000000000000000000000006",
+  decimals: 18
+)
+
+Token.find_or_create_by!(
+  chain: base_chain,
+  symbol: "USDC",
+  name: "USD Coin",
+  contract_address: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+  decimals: 6
+)
+
+degen_token = Token.find_or_create_by!(
+  chain: base_chain,
+  symbol: "DEGEN",
+  name: "Degen",
+  contract_address: "0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed",
+  decimals: 18
+)
+
+TokenPair.find_or_create_by!(
+  chain: base_chain,
+  base_token: degen_token,
+  quote_token: weth_token
 )
