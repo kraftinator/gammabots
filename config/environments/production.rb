16a70fe671520fe7a1ff17b41fe8615eaf531289
Adam Kraft <adamkraft1@gmail.com>
2025-02-10 10:50:55 -0600

    Created Procfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
   # config.active_job.queue_name_prefix = "gammabots_production"
+  config.active_job.queue_adapter = :sidekiq
 
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
