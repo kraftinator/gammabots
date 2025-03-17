d1ffe8230128778569278979a9dfb6fe67515ea9
Adam Kraft <adamkraft1@gmail.com>
2025-03-17 10:10:53 -0500

    Group bots by token pair in scheduler

--- app/jobs/bot_scheduler_job.rb
+++ app/jobs/bot_scheduler_job.rb
@@ -2,9 +2,17 @@ class BotSchedulerJob
   include Sidekiq::Job
 
   def perform
-    Bot.active.each do |bot|
-    #Bot.all.each do |bot|
-      BotRunnerJob.perform_async(bot.id)
+    bots_by_token_pair = Bot.active.group_by(&:token_pair_id)
+
+    bots_by_token_pair.each do |token_pair_id, bots|
+      next unless token_pair_id
+
+      token_pair = TokenPair.find(token_pair_id)
+      token_pair.latest_price
+
+      bots.each do |bot|
+        BotRunnerJob.perform_async(bot.id)
+      end
     end
   end
 end
