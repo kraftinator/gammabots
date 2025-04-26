71c864a7d8d084cb4584ec868916cba94e23ce5c
Adam Kraft <adamkraft1@gmail.com>
2025-04-26 13:52:38 -0500

    Added ssl_params to redis initializer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,4 +2,7 @@
 require 'redis'
 
 redis_url = ENV.fetch('REDIS_URL') { 'redis://localhost:6379/0' }
-$redis = Redis.new(url: redis_url)
+$redis = Redis.new(
+  url: redis_url,
+  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+)
