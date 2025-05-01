1098be54858497326287a917516c9265a12ecf07
Adam Kraft <adamkraft1@gmail.com>
2025-05-01 13:38:29 -0500

    Create redlock client

--- config/initializers/redlock.rb
+++ config/initializers/redlock.rb
@@ -1,5 +1,16 @@
 # config/initializers/redlock.rb
 require "redlock"
 
-# Use your existing $redis connection
-REDLOCK_CLIENT = Redlock::Client.new([ $redis ])
+servers = [
+  {
+    url:       ENV.fetch("REDIS_URL") { "redis://localhost:6379/0" },
+    ssl_params:{ verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
+]
+
+REDLOCK_CLIENT = Redlock::Client.new(
+  servers,
+  retry_count:  10,    # ~1s of retries (10×100ms)
+  retry_delay:  200,   # ms between retries
+  lock_ttl:     2_000  # 2s lock lifetime
+)
